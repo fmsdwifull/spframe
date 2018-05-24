56e6ff01cec138c908d531f3dae7c8164f7c5d19
fmsdwifull <fmsdwifull@sina.com>
2018-05-24 10:53:26 +0800

    之前git后缺少文件，有些文件git为何不跟踪呢

--- config/routes.php
+++ config/routes.php
@@ -2,7 +2,7 @@
 
 use NoahBuscher\Macaw\Macaw;
 
-Macaw::get('/comptst/public/index.php/xxx', function() {
+Macaw::get('/spframe/public/index.php/xxx', function() {
   echo "成功！";
 });
 
@@ -10,9 +10,9 @@ Macaw::get('(:all)', function($fu) {
   echo '未匹配到路由<br>'.$fu;
 });
 
-Macaw::get('/comptst/public/index.php/home', 'HomeController@home');
-Macaw::get('/comptst/public/index.php/home/kk', 'HomeController@kk');
-Macaw::get('/comptst/public/index.php/mredis', 'MredisController@index');
-Macaw::get('/comptst/public/index.php/mredis/connect', 'MredisController@connect');
+Macaw::get('/spframe/public/index.php/home', 'HomeController@home');
+Macaw::get('/spframe/public/index.php/home/kk', 'HomeController@kk');
+Macaw::get('/spframe/public/index.php/mredis', 'MredisController@index');
+Macaw::get('/spframe/public/index.php/mredis/connect', 'MredisController@connect');
 
 Macaw::dispatch();
--- vendor/composer/autoload_static.php
+++ vendor/composer/autoload_static.php
@@ -7,8 +7,9 @@ namespace Composer\Autoload;
 class ComposerStaticInit26bd6dd30f22ca51727a3f355113c061
 {
     public static $files = array (
-        '0e6d7bf4a5811bfa5cf40c5ccd6fae6a' => __DIR__ . '/..' . '/symfony/polyfill-mbstring/bootstrap.php',
+        '320cde22f66dd4f5d3fd621d3e88b98f' => __DIR__ . '/..' . '/symfony/polyfill-ctype/bootstrap.php',
         '3b5531f8bb4716e1b6014ad7e734f545' => __DIR__ . '/..' . '/illuminate/support/Illuminate/Support/helpers.php',
+        '0e6d7bf4a5811bfa5cf40c5ccd6fae6a' => __DIR__ . '/..' . '/symfony/polyfill-mbstring/bootstrap.php',
     );
 
     public static $prefixLengthsPsr4 = array (
@@ -23,6 +24,7 @@ class ComposerStaticInit26bd6dd30f22ca51727a3f355113c061
         'S' => 
         array (
             'Symfony\\Polyfill\\Mbstring\\' => 26,
+            'Symfony\\Polyfill\\Ctype\\' => 23,
             'Symfony\\Component\\Yaml\\' => 23,
             'Symfony\\Component\\Translation\\' => 30,
         ),
@@ -50,6 +52,10 @@ class ComposerStaticInit26bd6dd30f22ca51727a3f355113c061
         'Symfony\\Polyfill\\Mbstring\\' => 
         array (
             0 => __DIR__ . '/..' . '/symfony/polyfill-mbstring',
+        ),
+        'Symfony\\Polyfill\\Ctype\\' => 
+        array (
+            0 => __DIR__ . '/..' . '/symfony/polyfill-ctype',
         ),
         'Symfony\\Component\\Yaml\\' => 
         array (
